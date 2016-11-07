566c79067382ffed8d56fe7eb424f322c53fac34
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2016-11-07 10:12:06 +0200

    add cpp project, generate reals data

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ucu-adc-coursework
+## cpp-sources
+C++ code is writed in [Code Blocks](http://www.codeblocks.org/) and compiled by [GCC](http://gcc.gnu.org/)
---
+++ cpp-sources/reals-generator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ofstream realsFile;
+    realsFile.open ("../data/reals");
+
+    srand((unsigned)time(NULL));
+
+    for (int i = 0; i < 10000; i++ ){
+        double r = ((double)rand() / (double)(RAND_MAX));
+        realsFile << r << endl;
+    }
+
+    realsFile.close();
+
+    return 0;
+}
