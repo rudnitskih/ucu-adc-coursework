c36ec13296bfaa071efc9c0ca0be49b69e9dd07f
Anatoliy Stegniy <tsdaemon@gmail.com>
2016-12-20 13:57:46 +0200

    test data creation and testing

---
+++ graphs/negative-cycle/tests.py
@@ -0,0 +1,32 @@
+import utils as u
+
+# generate graph
+vertices, edges = u.generate_test_graph(100, 0.7)
+
+# generate negative cycle
+cycle = u.add_negative_cycle(vertices, edges)
+
+# check cycle is really negative
+s = 0
+for i in range(len(cycle)):
+    v1 = cycle[i]
+    next_key = i+1;
+    if i == len(cycle)-1:
+        next_key = 0
+    v2 = cycle[next_key]
+    weight = edges[u.gr_key(v1, v2)][2]
+    s += weight
+
+assert s < 0
+
+# save graph to file
+u.save_graph_to_file(vertices, edges, "1.graph")
+
+import numpy.testing as tst
+# read and check it the same
+vertices2, edges2 = u.read_graph_from_file("1.graph")
+assert len(vertices) == len(vertices2)
+for key in edges.keys():
+    assert edges[key][0] == edges2[key][0]
+    assert edges[key][1] == edges2[key][1]
+    tst.assert_approx_equal(edges[key][2], edges2[key][2], 4)
---
+++ graphs/negative-cycle/utils.py
@@ -0,0 +1,72 @@
+from random import uniform, randint
+
+
+def generate_test_graph(n, connectivity):
+    edges = {}
+    vertices = range(n)
+    for v1 in range(n):
+        for v2 in range(v1, n):
+            if uniform(0, 1) <= connectivity:
+                weight = uniform(-10, 10)
+                key = gr_key(v1, v2);
+                edges[key] = (v1, v2, weight)
+    return vertices, edges
+
+
+def add_negative_cycle(vertices, edges):
+    n = len(vertices)
+
+    # select k random vertices
+    k = randint(0, n/4)
+    cycle = []
+    for i in range(k):
+        random_vertice = randint(0, n)
+        while random_vertice in cycle:
+            random_vertice = randint(0, n)
+
+        cycle.append(random_vertice)
+    edges_cycle = []
+
+    # check if they have cycle and add edges if needed
+    for i in range(len(cycle)-1):
+        v1 = cycle[i]
+        v2 = cycle[i+1]
+        key = gr_key(v1, v2);
+        if not key in edges:
+            edges[key] = (v1,v2,uniform(-10, 10))
+        edges_cycle.append(edges[key][2])
+
+    # calculate weight sum and set last edge so that whole cycle will be negative
+    cycle_sum = sum(edges_cycle)
+    last_edge_weight = uniform(-cycle_sum-10, -cycle_sum-1)
+    last_edge_key = gr_key(cycle[-1], cycle[0])
+    edges[last_edge_key] = (cycle[-1], cycle[0], last_edge_weight)
+    return cycle
+
+
+def save_graph_to_file(vertices, edges, file_name):
+    f = open(file_name, "w")
+    n = len(vertices)
+    f.write(str(n) + "\n")
+
+    for key, v in edges.items():
+        f.write("%d %d %f\n" % (v[0], v[1], v[2]))
+    f.close()
+
+
+def read_graph_from_file(file_name):
+    f = open(file_name, "r")
+    n = int(f.readline());
+    vertices = range(n)
+    edges = {}
+    for line in f:
+        v = line.split(" ")
+        v1 = int(v[0])
+        v2 = int(v[1])
+        weight = float(v[2])
+        edges[gr_key(v[0], v[1])] = (v1,v2,weight)
+    return vertices, edges
+
+
+def gr_key(v1, v2):
+    return str(v1) + "_" + str(v2)
